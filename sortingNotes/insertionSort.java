d514772e561d349215a8ecbaaac08b6f18fbf910
HawkCS50 <hawkinda24@gmail.com>
2024-03-19 10:56:08 -0400

    endoc

---
+++ sortingNotes/insertionSort.java
@@ -0,0 +1,28 @@
+package sortingNotes;
+
+public class insertionSort {
+	public static void main(String[] args) {
+		int[] array = {5, 2, 8, 3, 1, 6};
+
+		// Print the original array using memory addresses
+		for (int i : array) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		sort(array);
+		for (int i : array) {
+			System.out.print(i + " ");
+		}
+	}
+	public static void sort(int[] something) {
+		for (int i= 0; i<something.length; i++) 
+			for (int j: something) {
+				if (something[i]<j) {
+					something[i]^=j;
+					j^=something[i];
+					something[i]^=j;
+				}
+			}
+	}
+
+}
